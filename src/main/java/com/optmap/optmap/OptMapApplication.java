1df50d4aa6d2ff18e9eac71861af5ef5c9805ef2
Nithiesh <nithiesh164@gmail.com>
2026-06-23 21:25:24 +0530

    Initial Commit

---
+++ src/main/java/com/optmap/optmap/OptMapApplication.java
@@ -0,0 +1,13 @@
+package com.optmap.optmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OptMapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/optmap/optmap/OptMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optmap.optmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
